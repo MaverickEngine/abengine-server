8c4801bcda247772c409103a8c6463b403e133c9
Jason Norwood-Young <jason@10layer.com>
2023-06-17 10:31:46 +0200

    Initial Sveltekit setup

---
+++ jedi-frontend/src/routes/docs/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <title>Documentation</title>
+    <meta name="description" content="Documentation for the Jedi project" />
+</svelte:head>
+
+<div class="text-column">
+    <h1>Hello Wrold</h1>
+</div>

019a9cd5dbc0828dc3e7156be5c8cd2aea06d68d
Jason Norwood-Young <jason@10layer.com>
2023-08-16 10:50:04 +0200

    Unused code

--- edji-frontend/src/hooks.server.js
+++ edji-frontend/src/hooks.server.js
@@ -1,13 +1,6 @@
 import { handleSession } from 'svelte-kit-cookie-session';
 import {SECRET_KEY} from '$env/static/private'
 
-function redirect(location, body) {
-    return new Response(body, {
-        status: 303,
-        headers: { location }
-    });
-}
-
 export const handle = handleSession({
 	secret: SECRET_KEY,
 });

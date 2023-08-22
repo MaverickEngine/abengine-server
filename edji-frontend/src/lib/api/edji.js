eafceeac94656688c315e7fd4a59ada83de867a2
Jason Norwood-Young <jason@10layer.com>
2023-08-22 10:44:55 +0200

    get_collections and get_collection

--- edji-frontend/src/lib/api/edji.js
+++ edji-frontend/src/lib/api/edji.js
@@ -9,3 +9,24 @@ export async function check_status() {
         throw "Error checking status - API could be down.";
     }
 }
+
+export async function get_collections() {
+    try {
+        console.log(`${BACKEND}/model`);
+        const res = await fetch(`${BACKEND}/model`);
+        return await res.json();
+    } catch(err) {
+        console.error(err);
+        throw "Error getting collections - API could be down.";
+    }
+}
+
+export async function get_collection(collection) {
+    try {
+        const res = await fetch(`${BACKEND}/model/${collection}`);
+        return await res.json();
+    } catch(err) {
+        console.error(err);
+        throw "Error getting collection - API could be down.";
+    }
+}

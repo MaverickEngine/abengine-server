c61c016e711db496f6fb8559d547c0141242e2f3
Jason Norwood-Young <jason@10layer.com>
2023-08-28 11:33:55 +0200

    Start of Explorer feature

--- edji-frontend/src/routes/(authed)/explorer/+page.svelte
+++ edji-frontend/src/routes/(authed)/explorer/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+    export let data;
+    console.log(data);
+</script>
+
 <h1>Explorer</h1>
+{#each data.collections as collection}
+    <p><a href="/explorer/{collection.model}">{collection.model}</a></p>
+{/each}

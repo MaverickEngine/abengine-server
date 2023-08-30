58e2b884f1600beb3438d176cb07c62f96cd6ee9
Jason Norwood-Young <jason@10layer.com>
2023-08-30 10:18:58 +0200

    sm, md and lg button versions

--- edji-frontend/src/lib/ui/button.svelte
+++ edji-frontend/src/lib/ui/button.svelte
@@ -1,13 +1,20 @@
 <script>
 	export let type = 'button';
 	export let disabled = false;
+	export let size = 'md';
+
+	const sizes = {
+		sm: 'py-1 px-2 text-sm',
+		md: 'py-2 px-4 text-md',
+		lg: 'py-3 px-6 text-lg',
+	};
 </script>
 
 <button {disabled}
 	{type}
 	class:cursor-not-allowed = {disabled}
 	class:opacity-50 = {disabled}
-	class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+	class="bg-blue-500 text-white font-bold py-2 px-4 rounded {sizes[size]}"
 >
 	<slot />
 </button>
